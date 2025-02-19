04aef931f87fb022b68f55b81e71a7e37d0d66b8
SantiagoBerdecio <santiberdecio@gmail.com>
2025-02-19 20:24:12 +0100

    Rama de Suma subida

---
+++ SumaTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumaTest {
+
+    @org.junit.jupiter.api.Test
+    void sumaDosReales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaDosEnteros() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaTresReales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void acumulador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resetAcumulador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void menuSuma() {
+    }
+}
